78bbb774e4f0a65c820032894816275baeb58908
d9nich <dan.galaiko@gmail.com>
2020-08-14 15:57:16 +0300

    Initial commit

---
+++ src/test/java/com/d9nich/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.d9nich.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
